9a129852561e2c79971d67bfbf2ad3de827b2516
Alfonso Eduardo Carrillo Paredes <alfonso.carrillo@revimex.mx>
2019-08-09 17:51:38 -0500

    Rest server - usuario

--- server/server.js
+++ server/server.js
@@ -3,50 +3,23 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
- 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario')
-})
 
-app.post('/usuario', function (req, res) {
+app.use(require('./routes/usuario'));
 
-    let body = req.body;
+mongoose.connect(process.env.URLDB,
+    {useNewUrlParser: true, useCreateIndex: true},
+    (err, res) => {
+    if (err) throw err;
 
-    if (body.nombre === undefined) {
-        
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        
-        res.json({
-            persona: body
-        });
-
-    }
-
-})
-
-app.put('/usuario/:id', function (req, res) {
-    
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-})
+    console.log('Base de datos CONNECTED');
+});
  
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
